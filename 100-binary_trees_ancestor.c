026a8b6dca532790c0a02e51c2896c86f227283b
martinmsaavedra <martin@bildung.uy>
2021-03-10 18:21:55 +0000

    adv task

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -2,32 +2,5 @@
 
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
 {
-	const binary_tree_t *curr = first, *right_subtree = NULL, *left_subtree = NULL;
-
-	if (curr == NULL)
-	{
-		return NULL;
-	}
-	else if (curr == first || curr == second)
-	{ 
-		return curr;
-	}
 	
-	left_subtree = lowest_common_ancestor(curr->left, first, second);
-	right_subtree = lowest_common_ancestor(curr->right, first, second);
-
-	if (left_subtree != NULL && right_subtree != NULL)
-	{ 
-		return curr;
-	}
-	else if (left_subtree != NULL)
-	{
-		return left_subtree;
-	}
-	else
-	{
-		return right_subtree;
-	}
-
-	return NULL; 
 }
