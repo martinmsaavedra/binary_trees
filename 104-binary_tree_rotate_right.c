e6aad9cad49d6a9695b797abc5b7c899b30a888c
martinmsaavedra <martin@bildung.uy>
2021-03-11 00:18:43 +0000

    big O

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -10,7 +10,7 @@ binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
 	binary_tree_t *y = NULL, *x = NULL;
 
-	if (!tree || tree->left == NULL)
+	if (!tree)
 		return (NULL);
 
 	y = tree->left;
---
+++ 110-binary_tree_is_bst.c
@@ -0,0 +1,9 @@
+#include "binary_trees.h"
+
+int binary_tree_is_bst(const binary_tree_t *tree)
+{
+    if (!tree)
+        return (NULL);
+    
+    
+}
